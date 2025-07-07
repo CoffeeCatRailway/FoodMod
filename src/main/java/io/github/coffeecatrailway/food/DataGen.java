d9fc4db533104ba7a730e6d23025fd0de8aac611
CoffeeCatRailway <dragonlee1002@gmail.com>
2025-07-07 16:50:00 +1000

    tools

--- src/main/java/io/github/coffeecatrailway/food/DataGen.java
+++ src/main/java/io/github/coffeecatrailway/food/DataGen.java
@@ -3,7 +3,6 @@ package io.github.coffeecatrailway.food;
 import io.github.coffeecatrailway.food.common.ModFoods;
 import io.github.coffeecatrailway.food.common.item.ModItems;
 import io.github.coffeecatrailway.food.common.item.crafting.SandwichRecipe;
-import io.github.coffeecatrailway.food.config.FoodConfigClient;
 import io.github.coffeecatrailway.food.config.FoodConfigs;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
@@ -12,6 +11,7 @@ import net.minecraft.data.recipes.*;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
@@ -21,6 +21,7 @@ import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
 import net.neoforged.neoforge.common.ModConfigSpec;
+import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.common.data.LanguageProvider;
@@ -44,8 +45,18 @@ public class DataGen
 		@Override
 		protected void registerModels()
 		{
-			this.basicItem(ModItems.BREAD_SLICE.get());
-			this.basicItem(ModItems.TOAST.get());
+			// Crafting Materials
+			this.basicItem(ModItems.GEAR_WOODEN.get());
+			this.basicItem(ModItems.GEAR_STONE.get());
+
+			// Tools
+			this.basicItem(ModItems.CURDLER_WOODEN.get());
+			this.basicItem(ModItems.CURDLER_STONE.get());
+
+			this.basicItem(ModItems.ROLLING_PIN_WOODEN.get());
+			this.basicItem(ModItems.ROLLING_PIN_STONE.get());
+
+			this.basicItem(ModItems.GRIND_STONES.get());
 
 			this.basicItem(ModItems.KNIFE_WOODEN.get());
 			this.basicItem(ModItems.KNIFE_STONE.get());
@@ -54,6 +65,10 @@ public class DataGen
 			this.basicItem(ModItems.KNIFE_IRON.get());
 			this.basicItem(ModItems.KNIFE_DIAMOND.get());
 			this.basicItem(ModItems.KNIFE_NETHERITE.get());
+
+			// Foods
+			this.basicItem(ModItems.BREAD_SLICE.get());
+			this.basicItem(ModItems.TOAST.get());
 		}
 	}
 
@@ -67,8 +82,18 @@ public class DataGen
 		@Override
 		protected void addTranslations()
 		{
-			this.add(ModItems.BREAD_SLICE.get(), "Bread Slice");
-			this.add(ModItems.TOAST.get(), "Toast");
+			// Crafting Materials
+			this.add(ModItems.GEAR_WOODEN.get(), "Wooden Gear");
+			this.add(ModItems.GEAR_STONE.get(), "Stone Gear");
+
+			// Tools
+			this.add(ModItems.CURDLER_WOODEN.get(), "Wooden Curdler");
+			this.add(ModItems.CURDLER_STONE.get(), "Stone Curdler");
+
+			this.add(ModItems.ROLLING_PIN_WOODEN.get(), "Wooden Rolling Pin");
+			this.add(ModItems.ROLLING_PIN_STONE.get(), "Stone Rolling Pin");
+
+			this.add(ModItems.GRIND_STONES.get(), "Grind Stones");
 
 			this.add(ModItems.KNIFE_WOODEN.get(), "Wooden Knife");
 			this.add(ModItems.KNIFE_STONE.get(), "Stone Knife");
@@ -78,14 +103,20 @@ public class DataGen
 			this.add(ModItems.KNIFE_DIAMOND.get(), "Diamond Knife");
 			this.add(ModItems.KNIFE_NETHERITE.get(), "Netherite Knife");
 
+			// Foods
+			this.add(ModItems.BREAD_SLICE.get(), "Bread Slice");
+			this.add(ModItems.TOAST.get(), "Toast");
+
 			this.add("item." + FoodMod.MODID + ".food_combo.info", "to show ingredients");
 			this.add("item." + FoodMod.MODID + ".food_combo.info.nutrition", "Nutrition: %s");
 			this.add("item." + FoodMod.MODID + ".food_combo.info.saturation", "Saturation: %s");
 			this.add(ModItems.SANDWICH.get(), "Sandwich");
 			this.add("item." + FoodMod.MODID + ".sandwich.toasted", "Toasted");
 
+			// Other
 			this.add("tooltip." + FoodMod.MODID + ".hold_shift", "[HOLD SHIFT %s]");
 
+			// Config
 			this.add(FoodMod.MODID + ".configuration.sandwich", "Sandwich");
 			this.addConfig(FoodConfigs.CLIENT.rotateIngredients, "Rotate Ingredients");
 			this.addConfig(FoodConfigs.CLIENT.showNutritionSaturation, "Show Nutrition/Saturation");
@@ -105,7 +136,7 @@ public class DataGen
 
 		public static MutableComponent shiftInfo(Object info)
 		{
-			return Component.translatable( "tooltip." + FoodMod.MODID + ".hold_shift", info);
+			return Component.translatable("tooltip." + FoodMod.MODID + ".hold_shift", info);
 		}
 	}
 
@@ -119,19 +150,42 @@ public class DataGen
 		@Override
 		protected void buildRecipes(RecipeOutput recipeOutput)
 		{
-			ShapelessRecipeBuilder.shapeless(RecipeCategory.FOOD, ModItems.BREAD_SLICE, ModFoods.SLICE_PER_BREAD).requires(Items.BREAD).requires(ItemTag.KNIFES).unlockedBy("has_bread", has(Items.BREAD)).save(recipeOutput);
+			// Crafting Materials
+			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.GEAR_WOODEN).pattern(" s ").pattern("sms").pattern(" s ").define('m', ItemTags.PLANKS).define('s', Tags.Items.RODS_WOODEN)
+					.showNotification(false).group("gear").unlockedBy("criteria", has(ItemTags.PLANKS)).save(recipeOutput);
+			ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.GEAR_STONE).pattern(" s ").pattern("sms").pattern(" s ").define('m', Tags.Items.STONES).define('s', Tags.Items.RODS_WOODEN)
+					.showNotification(false).group("gear").unlockedBy("criteria", has(Tags.Items.STONES)).save(recipeOutput);
+
+			// Tools
+			ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.CURDLER_WOODEN).pattern("s").pattern("s").pattern("g").define('g', ItemTag.GEARS_WOODEN).define('s', Tags.Items.RODS_WOODEN)
+					.showNotification(false).group("curdler").unlockedBy("criteria", has(ItemTag.GEARS_WOODEN)).save(recipeOutput);
+			ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.CURDLER_STONE).pattern("s").pattern("s").pattern("g").define('g', ItemTag.GEARS_STONE).define('s', Tags.Items.RODS_WOODEN)
+					.showNotification(false).group("curdler").unlockedBy("criteria", has(ItemTag.GEARS_STONE)).save(recipeOutput);
+
+			ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.ROLLING_PIN_WOODEN).pattern("sms").define('m', ItemTags.PLANKS).define('s', Tags.Items.RODS_WOODEN)
+					.showNotification(false).group("rolling_pin").unlockedBy("criteria", has(ItemTags.PLANKS)).save(recipeOutput);
+			ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.ROLLING_PIN_STONE).pattern("sms").define('m', Tags.Items.STONES).define('s', Tags.Items.RODS_WOODEN)
+					.showNotification(false).group("rolling_pin").unlockedBy("criteria", has(Tags.Items.STONES)).save(recipeOutput);
+
+			ShapelessRecipeBuilder.shapeless(RecipeCategory.TOOLS, ModItems.GRIND_STONES).requires(Items.COBBLESTONE).unlockedBy("has_stone", has(Items.COBBLESTONE)).save(recipeOutput);
+
+			this.knife(ModItems.KNIFE_WOODEN, ItemTags.PLANKS, recipeOutput);
+			this.knife(ModItems.KNIFE_STONE, Tags.Items.STONES, recipeOutput);
+			this.knife(ModItems.KNIFE_COPPER, Items.COPPER_INGOT, recipeOutput);
+			this.knife(ModItems.KNIFE_GOLD, Tags.Items.INGOTS_GOLD, recipeOutput);
+			this.knife(ModItems.KNIFE_IRON, Tags.Items.INGOTS_IRON, recipeOutput);
+			this.knife(ModItems.KNIFE_DIAMOND, Tags.Items.GEMS_DIAMOND, recipeOutput);
+			SmithingTransformRecipeBuilder.smithing(Ingredient.of(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE), Ingredient.of(ModItems.KNIFE_DIAMOND), Ingredient.of(Tags.Items.INGOTS_NETHERITE), RecipeCategory.TOOLS, ModItems.KNIFE_NETHERITE.get())
+					.unlocks("has_netherite", has(Tags.Items.INGOTS_NETHERITE)).save(recipeOutput, FoodMod.id("knife_netherite"));
+
+			// Foods
+			ShapelessRecipeBuilder.shapeless(RecipeCategory.FOOD, ModItems.BREAD_SLICE, ModFoods.SLICE_PER_BREAD).requires(Items.BREAD).requires(ItemTag.TOOLS_KNIFE).unlockedBy("has_bread", has(Items.BREAD)).save(recipeOutput);
 
 			this.cookedFood(ModItems.BREAD_SLICE, ModItems.TOAST, .35f, 200, recipeOutput);
 
 			SpecialRecipeBuilder.special(SandwichRecipe::new).save(recipeOutput, FoodMod.id("sandwich"));
 
-			this.knife(ModItems.KNIFE_WOODEN, ItemTags.PLANKS, recipeOutput);
-			this.knife(ModItems.KNIFE_STONE, Items.STONE, recipeOutput);
-			this.knife(ModItems.KNIFE_COPPER, Items.COPPER_INGOT, recipeOutput);
-			this.knife(ModItems.KNIFE_GOLD, Items.GOLD_INGOT, recipeOutput);
-			this.knife(ModItems.KNIFE_IRON, Items.IRON_INGOT, recipeOutput);
-			this.knife(ModItems.KNIFE_DIAMOND, Items.DIAMOND, recipeOutput);
-			SmithingTransformRecipeBuilder.smithing(Ingredient.of(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE), Ingredient.of(ModItems.KNIFE_DIAMOND), Ingredient.of(Items.NETHERITE_INGOT), RecipeCategory.TOOLS, ModItems.KNIFE_NETHERITE.get()).unlocks("has_netherite", has(Items.NETHERITE_INGOT)).save(recipeOutput, FoodMod.id("knife_netherite"));
+			// Other
 		}
 
 		private void cookedFood(ItemLike raw, ItemLike cooked, float experience, int baseCookTime, RecipeOutput recipeOutput)
@@ -153,7 +207,7 @@ public class DataGen
 
 		private ShapedRecipeBuilder knifeBuilder(ItemLike result)
 		{
-			return ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, result).pattern("m").pattern("s").define('s', Items.STICK).showNotification(true).group("knife");
+			return ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, result).pattern("m").pattern("s").define('s', Tags.Items.RODS_WOODEN).showNotification(true).group("knife");
 		}
 	}
 
@@ -172,7 +226,17 @@ public class DataGen
 
 	public static class ItemTag extends ItemTagsProvider
 	{
-		public static final TagKey<Item> KNIFES = TagKey.create(Registries.ITEM, FoodMod.id("knifes"));
+		// Crafting Materials
+		public static final TagKey<Item> GEARS_WOODEN = tagCommon("gears/wooden");
+		public static final TagKey<Item> GEARS_STONE = tagCommon("gears/stone");
+		public static final TagKey<Item> GEARS = tagCommon("gears");
+
+		// Tools
+		public static final TagKey<Item> TOOLS_CURDLER = tag("tools/curdler");
+		public static final TagKey<Item> TOOLS_ROLLING_PIN = tag("tools/rolling_pin");
+		public static final TagKey<Item> TOOLS_KNIFE = tag("tools/knife");
+
+		// Foods
 		public static final TagKey<Item> BREAD_SLICE = TagKey.create(Registries.ITEM, FoodMod.id("bread_slice"));
 
 		public ItemTag(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, CompletableFuture<TagLookup<Block>> blockTagLookup, @Nullable ExistingFileHelper existingFileHelper)
@@ -183,8 +247,29 @@ public class DataGen
 		@Override
 		protected void addTags(HolderLookup.Provider provider)
 		{
-			this.tag(KNIFES).add(ModItems.KNIFE_WOODEN.get(), ModItems.KNIFE_STONE.get(), ModItems.KNIFE_COPPER.get(), ModItems.KNIFE_GOLD.get(), ModItems.KNIFE_IRON.get(), ModItems.KNIFE_DIAMOND.get(), ModItems.KNIFE_NETHERITE.get());
+			// Crafting Materials
+			this.tag(GEARS_WOODEN).add(ModItems.GEAR_WOODEN.get());
+			this.tag(GEARS_STONE).add(ModItems.GEAR_STONE.get());
+			this.tag(GEARS).addTags(GEARS_WOODEN, GEARS_STONE);
+
+			// Tools
+			this.tag(TOOLS_CURDLER).add(ModItems.CURDLER_WOODEN.get(), ModItems.CURDLER_STONE.get());
+			this.tag(TOOLS_ROLLING_PIN).add(ModItems.ROLLING_PIN_WOODEN.get(), ModItems.ROLLING_PIN_STONE.get());
+			this.tag(TOOLS_KNIFE).add(ModItems.KNIFE_WOODEN.get(), ModItems.KNIFE_STONE.get(), ModItems.KNIFE_COPPER.get(), ModItems.KNIFE_GOLD.get(), ModItems.KNIFE_IRON.get(), ModItems.KNIFE_DIAMOND.get(), ModItems.KNIFE_NETHERITE.get());
+			this.tag(Tags.Items.TOOLS).addTags(TOOLS_CURDLER, TOOLS_ROLLING_PIN, TOOLS_KNIFE);
+
+			// Foods
 			this.tag(BREAD_SLICE).add(ModItems.BREAD_SLICE.get(), ModItems.TOAST.get());
+		}
+
+		private static TagKey<Item> tag(String location)
+		{
+			return TagKey.create(Registries.ITEM, FoodMod.id(location));
+		}
+
+		private static TagKey<Item> tagCommon(String location)
+		{
+			return TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("c", location));
 		}
 	}
 }
--- src/main/java/io/github/coffeecatrailway/food/common/item/ModItems.java
+++ src/main/java/io/github/coffeecatrailway/food/common/item/ModItems.java
@@ -19,18 +19,32 @@ public class ModItems
 	private static final Logger LOGGER = LogUtils.getLogger();
 	public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(FoodMod.MODID);
 
+	// Crafting Materials
+	public static final DeferredItem<Item> GEAR_WOODEN = ITEMS.registerSimpleItem("gear_wooden");
+	public static final DeferredItem<Item> GEAR_STONE = ITEMS.registerSimpleItem("gear_stone");
+
+	// Tools
+	public static final DeferredItem<CraftingToolItem> CURDLER_WOODEN = ITEMS.registerItem("curdler_wooden", prop -> itemCraftingTool(prop, Tiers.WOOD, 2f, -2.5f));
+	public static final DeferredItem<CraftingToolItem> CURDLER_STONE = ITEMS.registerItem("curdler_stone", prop -> itemCraftingTool(prop, Tiers.STONE, 2f, -2.5f));
+
+	public static final DeferredItem<CraftingToolItem> ROLLING_PIN_WOODEN = ITEMS.registerItem("rolling_pin_wooden", prop -> itemCraftingTool(prop, Tiers.WOOD, 1f, -2.5f));
+	public static final DeferredItem<CraftingToolItem> ROLLING_PIN_STONE = ITEMS.registerItem("rolling_pin_stone", prop -> itemCraftingTool(prop, Tiers.STONE, 1f, -2.5f));
+
+	public static final DeferredItem<CraftingToolItem> GRIND_STONES = ITEMS.registerItem("grind_stones", prop -> itemCraftingTool(prop, Tiers.STONE, 2f, -2.0f));
+
+	public static final DeferredItem<CraftingToolItem> KNIFE_WOODEN = ITEMS.registerItem("knife_wooden", prop -> itemCraftingTool(prop, Tiers.WOOD, 1f, -2.5f));
+	public static final DeferredItem<CraftingToolItem> KNIFE_STONE = ITEMS.registerItem("knife_stone", prop -> itemCraftingTool(prop, Tiers.STONE, 1f, -2.5f));
+	public static final DeferredItem<CraftingToolItem> KNIFE_COPPER = ITEMS.registerItem("knife_copper", prop -> itemCraftingTool(prop, Tiers.STONE, 1f, -2.5f));
+	public static final DeferredItem<CraftingToolItem> KNIFE_GOLD = ITEMS.registerItem("knife_gold", prop -> itemCraftingTool(prop, Tiers.GOLD, 1f, -2.5f));
+	public static final DeferredItem<CraftingToolItem> KNIFE_IRON = ITEMS.registerItem("knife_iron", prop -> itemCraftingTool(prop, Tiers.IRON, 1f, -2.5f));
+	public static final DeferredItem<CraftingToolItem> KNIFE_DIAMOND = ITEMS.registerItem("knife_diamond", prop -> itemCraftingTool(prop, Tiers.DIAMOND, 1f, -2.5f));
+	public static final DeferredItem<CraftingToolItem> KNIFE_NETHERITE = ITEMS.registerItem("knife_netherite", prop -> itemCraftingTool(prop, Tiers.NETHERITE, 1f, -2.5f));
+
+	// Foods
 	public static final DeferredItem<Item> BREAD_SLICE = ITEMS.registerSimpleItem("bread_slice", new Item.Properties().food(ModFoods.FOOD_BREAD_SLICE));
 	public static final DeferredItem<Item> TOAST = ITEMS.registerSimpleItem("toast", new Item.Properties().food(ModFoods.FOOD_TOAST));
 
 	public static final DeferredItem<SandwichItem> SANDWICH = ITEMS.registerItem("sandwich", SandwichItem::new);
-
-	public static final DeferredItem<CraftingToolItem> KNIFE_WOODEN = ITEMS.registerItem("knife_wooden", prop -> itemCraftingTool(prop, Tiers.WOOD, 1.f, -2.5f));
-	public static final DeferredItem<CraftingToolItem> KNIFE_STONE = ITEMS.registerItem("knife_stone", prop -> itemCraftingTool(prop, Tiers.STONE, 1.f, -2.5f));
-	public static final DeferredItem<CraftingToolItem> KNIFE_COPPER = ITEMS.registerItem("knife_copper", prop -> itemCraftingTool(prop, Tiers.STONE, 1.f, -2.5f));
-	public static final DeferredItem<CraftingToolItem> KNIFE_GOLD = ITEMS.registerItem("knife_gold", prop -> itemCraftingTool(prop, Tiers.GOLD, 1.f, -2.5f));
-	public static final DeferredItem<CraftingToolItem> KNIFE_IRON = ITEMS.registerItem("knife_iron", prop -> itemCraftingTool(prop, Tiers.IRON, 1.f, -2.5f));
-	public static final DeferredItem<CraftingToolItem> KNIFE_DIAMOND = ITEMS.registerItem("knife_diamond", prop -> itemCraftingTool(prop, Tiers.DIAMOND, 1.f, -2.5f));
-	public static final DeferredItem<CraftingToolItem> KNIFE_NETHERITE = ITEMS.registerItem("knife_netherite", prop -> itemCraftingTool(prop, Tiers.NETHERITE, 1.f, -2.5f));
 
 	private static CraftingToolItem itemCraftingTool(Item.Properties prop, Tier tier, float attackDamage, float attackSpeed)
 	{
@@ -47,21 +61,45 @@ public class ModItems
 
 	private static void addCreative(BuildCreativeModeTabContentsEvent event)
 	{
+		if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES)
+		{
+			LOGGER.info("Adding tool items to tabs");
+			final ItemStack WOODEN_HOE = new ItemStack(Items.WOODEN_HOE);
+			final ItemStack STONE_HOE = new ItemStack(Items.STONE_HOE);
+			final ItemStack GOLDEN_HOE = new ItemStack(Items.GOLDEN_HOE);
+			final ItemStack IRON_HOE = new ItemStack(Items.IRON_HOE);
+			final ItemStack DIAMOND_HOE = new ItemStack(Items.DIAMOND_HOE);
+			final ItemStack NETHERITE_HOE = new ItemStack(Items.NETHERITE_HOE);
+
+			event.insertAfter(WOODEN_HOE, new ItemStack(CURDLER_WOODEN.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+			event.insertAfter(STONE_HOE, new ItemStack(CURDLER_STONE.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+
+			event.insertAfter(WOODEN_HOE, new ItemStack(ROLLING_PIN_WOODEN.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+			event.insertAfter(STONE_HOE, new ItemStack(ROLLING_PIN_STONE.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+
+			event.accept(GRIND_STONES);
+
+			event.insertAfter(WOODEN_HOE, new ItemStack(KNIFE_WOODEN.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+			event.insertAfter(STONE_HOE, new ItemStack(KNIFE_STONE.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+			event.insertAfter(new ItemStack(KNIFE_STONE.get()), new ItemStack(KNIFE_COPPER.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+			event.insertAfter(GOLDEN_HOE, new ItemStack(KNIFE_GOLD.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+			event.insertAfter(IRON_HOE, new ItemStack(KNIFE_IRON.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+			event.insertAfter(DIAMOND_HOE, new ItemStack(KNIFE_DIAMOND.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+			event.insertAfter(NETHERITE_HOE, new ItemStack(KNIFE_NETHERITE.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+		}
+
 		if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS)
 		{
 			LOGGER.info("Adding food items to tabs");
 			event.insertAfter(new ItemStack(Items.BREAD), new ItemStack(BREAD_SLICE.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
 			event.insertAfter(new ItemStack(BREAD_SLICE.get()), new ItemStack(TOAST.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-		} else if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES)
+		}
+
+		if (event.getTabKey() == CreativeModeTabs.INGREDIENTS)
 		{
-			LOGGER.info("Adding tool items to tabs");
-			event.insertAfter(new ItemStack(Items.WOODEN_HOE), new ItemStack(KNIFE_WOODEN.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-			event.insertAfter(new ItemStack(Items.STONE_HOE), new ItemStack(KNIFE_STONE.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-			event.insertAfter(new ItemStack(KNIFE_STONE.get()), new ItemStack(KNIFE_COPPER.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-			event.insertAfter(new ItemStack(Items.GOLDEN_HOE), new ItemStack(KNIFE_GOLD.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-			event.insertAfter(new ItemStack(Items.IRON_HOE), new ItemStack(KNIFE_IRON.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-			event.insertAfter(new ItemStack(Items.DIAMOND_HOE), new ItemStack(KNIFE_DIAMOND.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-			event.insertAfter(new ItemStack(Items.NETHERITE_HOE), new ItemStack(KNIFE_NETHERITE.get()), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+			LOGGER.info("Adding ingredient items to tabs");
+			event.accept(GEAR_WOODEN);
+			event.accept(GEAR_STONE);
 		}
 	}
 }
